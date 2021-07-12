98533dfe15afd3850c910070dadfee392b4675bc
Vvlado3 <72707803+vvlado3@users.noreply.github.com>
2021-07-12 10:37:04 +0300

    Update txadmin.sh

--- txAdmin & phpmyadmin/txadmin.sh
+++ txAdmin & phpmyadmin/txadmin.sh
@@ -1,5 +1,4 @@
-mkdir FXServer
-mkdir server
+mkdir FXServer/server
 cd FXServer/server
 wget https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/4022-2771986f973c6df2844eb907973a4ff1db90bde9/fx.tar.xz;
 tar xf fx.tar.xz
